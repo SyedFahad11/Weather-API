148a0af01352e07a05fcdf1d6364094cdfa51b66
SyedFahad11 <SYEDHU987@GMAIL.COM>
2024-05-10 22:01:32 +0530

    added logic for handling api requests

--- app.js
+++ app.js
@@ -6,6 +6,39 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html')
 })
+
+app.post('/', function(req, res){
+  console.log('Post Response here');
+  //url parts
+  const query = req.body.cityName;
+  const  apiKey = process.env.key;
+  console.log(apiKey);
+  const unit = 'metric';
+  //request from our Server to external server 'API'
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
+  https.get(url, function(response){
+
+    //response from external Server to our server
+    response.on('data', function(data){
+
+      console.log(data);
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      console.log(weatherData + temp + weatherDescription + icon + iconUrl);
+
+      //response from our server to client server
+      res.write(`<p>The weather description is ${weatherDescription}</p>`)
+      res.write(`<h1>The Tempereture in ${query} is ${temp} in Celcuis Degrees. </h1>`)
+      res.write(`<img src="${iconUrl}" />`)
+      res.send()
+    })
+  })
+
+})
+
 app.listen(3000, function(){
   console.log('Running server on port 3000.');
 })
